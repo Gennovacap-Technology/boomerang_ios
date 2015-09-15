7064bae6a31f18b43ff7b6e004b9fc9708eca5cc
Thales Pereira <thalesfp@gmail.com>
2015-09-15 20:14:59 -0300

    Add Waiting View

---
+++ Bangarang/App/Controllers/WaitingViewController.h
@@ -0,0 +1,15 @@
+//
+//  WaitingViewController.h
+//  Bangarang
+//
+//  Created by Thales Pereira on 9/15/15.
+//  Copyright (c) 2015 Gennovacap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaitingViewController : UIView
+
+@property (weak, nonatomic) IBOutlet UILabel *name;
+
+@end
---
+++ Bangarang/App/Controllers/WaitingViewController.m
@@ -0,0 +1,17 @@
+//
+//  WaitingViewController.m
+//  Bangarang
+//
+//  Created by Thales Pereira on 9/15/15.
+//  Copyright (c) 2015 Gennovacap. All rights reserved.
+//
+
+#import "WaitingViewController.h"
+
+@interface WaitingViewController ()
+
+@end
+
+@implementation WaitingViewController
+
+@end
