0e97f682d71d903d07ee66e17ca9c7a3257feca2
Thales Pereira <thalesfp@gmail.com>
2015-09-17 02:02:10 -0300

    Add Parse cloudcode

---
+++ cloudcode/cloud/main.js
@@ -0,0 +1,32 @@
+Parse.Cloud.beforeSave("Request", function(request, response) {
+	if (!request.object.isNew()) {
+      // Let existing object updates go through
+      response.success();
+  }
+
+	var fromQuery = new Parse.Query("Request");
+
+	fromQuery.equalTo("fromUser", request.object.get("fromUser"));
+	fromQuery.equalTo("toUser", request.object.get("toUser"));
+
+	var toQuery = new Parse.Query("Request");
+
+	toQuery.equalTo("fromUser", request.object.get("toUser"));
+	toQuery.equalTo("toUser", request.object.get("fromUser"));
+
+	var mainQuery = Parse.Query.or(fromQuery, toQuery);
+
+	mainQuery.first ({
+		success: function(object) {
+			if (object) {
+				response.error("Request already created.");
+			} else {
+				response.success();
+			}
+    },
+    error: function(error) {
+    	console.log(error.message);
+      response.success();
+    }
+  });
+});
