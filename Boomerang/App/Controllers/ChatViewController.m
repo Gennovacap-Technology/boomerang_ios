97836e8d0982c4dea6e5987be613378f60ab0416
Thales Pereira <thalesfp@gmail.com>
2015-09-24 22:29:58 -0300

    Add comments

--- Boomerang/App/Controllers/ChatViewController.m
+++ Boomerang/App/Controllers/ChatViewController.m
@@ -53,19 +53,20 @@
     UIImageView *backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"BackgroundLogin"]];
     [backgroundImage setContentMode:UIViewContentModeScaleAspectFit];
     
+    // Send Button
     UIButton *sendButton = [UIButton buttonWithType:UIButtonTypeCustom];
     UIImage *btnImage = [UIImage imageNamed:@"ButtonSend"];
     [sendButton setImage:btnImage forState:UIControlStateNormal];
     
     self.inputToolbar.contentView.rightBarButtonItem = sendButton;
     
+    // Text input bar
     [self.inputToolbar.contentView setBackgroundColor:[UIColor colorWithWhite:0.157 alpha:1.000]];
     [self.inputToolbar.contentView.textView setBackgroundColor:[UIColor colorWithWhite:0.157 alpha:1.000]];
     [self.inputToolbar.contentView.textView.layer setBorderColor:(__bridge CGColorRef _Nullable)([UIColor colorWithWhite:0.157 alpha:1.000])];
     [self.inputToolbar.contentView.textView setTextColor:[UIColor colorWithRed:1.000 green:0.000 blue:0.506 alpha:1.000]];
     [self.inputToolbar.contentView.textView setPlaceHolder:@"Type something..."];
     [self.inputToolbar.contentView.textView setPlaceHolderTextColor:[UIColor colorWithRed:1.000 green:0.000 blue:0.506 alpha:0.500]];
-    
     
     [self.collectionView.backgroundView addSubview:backgroundImage];
     
